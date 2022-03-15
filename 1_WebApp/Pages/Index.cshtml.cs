2392f34e3c9cb6acee672ee4f80239f73f4e2772
Maksim <maksalmak@gmail.com>
2022-03-15 10:22:27 +0000

    1 web app

---
+++ 1_WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    [ViewData]
+    public List<SomeData> DataList { get; set; } = new List<SomeData>();
+    public void OnGet()
+    {
+        var names = new Bogus.DataSets.Name();
+        DataList.AddRange( Enumerable
+            .Repeat(0, 10)
+            .Select(_ => new SomeData(Random.Shared.Next(100), names.FirstName()))
+        );
+
+    }
+}
+public record SomeData(int Age, string Name);
